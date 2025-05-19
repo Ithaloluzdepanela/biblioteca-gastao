105e13a6022ac60ac41fb6b3ae2eae0891c52295
Ithaloluzdepanela <ithalop48@gmail.com>
2025-05-19 20:29:36 -0300

    Classes

---
+++ BibliotecaGastao/emprestimo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace BibliotecaGastao.Models
+{
+    public class emprestimo
+
+    {
+        int id { get; set; }
+        int usuarioId { get; set; }
+        int livroId { get; set; }
+        DateAndTime dataEmprestimo { get; set; }
+        DateAndTime dataDevolucao { get; set; }
+        DateAndTime dataRealDevolucao { get; set; }
+        bool status { get; set; }
+
+    }
+}
---
+++ BibliotecaGastao/usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReaLTaiizor.Controls;
+
+namespace BibliotecaGastao
+{
+    public class usuario
+    {
+        int id { get; set; }
+        string nome { get; set; }
+        string email { get; set; }
+        string senha { get; set; }
+        string cpf { get; set; }
+        string data { get; set; }
+        string turma { get; set; }
+        int tel { get; set; }
+        char user { get; set; }
+    }
+}

b3488bec6cc1eee25aa1381ecb0bd35e7c2e027c
Ithaloluzdepanela <ithalop48@gmail.com>
2025-06-05 23:29:54 -0300

    Form usuarios

---
+++ BibliotecaApp/Usuarios.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string TipoUsuario { get; set; }
+    public string CPF { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+    public string Turma { get; set; }
+    public string Senha { get; set; }
+    public string ConfirmarSenha { get; set; }
+
+    // Lista compartilhada
+    public static List<Usuario> ListaUsuarios { get; set; } = new List<Usuario>();
+
+    public Usuario() { }
+
+    public Usuario(int id, string nome, string tipoUsuario, string cpf, DateTime dataNascimento,
+                   string telefone, string email, string turma, string senha, string confirmarSenha)
+    {
+        Id = id;
+        Nome = nome;
+        TipoUsuario = tipoUsuario;
+        CPF = cpf;
+        DataNascimento = dataNascimento;
+        Telefone = telefone;
+        Email = email;
+        Turma = turma;
+        Senha = senha;
+        ConfirmarSenha = confirmarSenha;
+    }
+
+    public bool SenhasCoincidem()
+    {
+        return Senha == ConfirmarSenha;
+    }
+
+    public bool CamposObrigatoriosPreenchidos()
+    {
+        return !string.IsNullOrWhiteSpace(Nome)
+            && !string.IsNullOrWhiteSpace(TipoUsuario)
+            && !string.IsNullOrWhiteSpace(CPF)
+            && !string.IsNullOrWhiteSpace(Senha)
+            && !string.IsNullOrWhiteSpace(ConfirmarSenha);
+    }
+}
